8d925594720d0d7d8688de72490bac0e65467f0a
Ego <tonglib@yonyou.com>
2019-07-19 11:27:06 +0800

    fix(router): modify /resume

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './views/Home.vue'
 import Resume from './views/Resume.vue'
 
 Vue.use(Router)
@@ -8,13 +9,13 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
     {
       path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/resume',
       name: 'resume',
       component: Resume
     },
